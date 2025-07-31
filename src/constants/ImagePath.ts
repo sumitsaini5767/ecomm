287e93105bdb3338a1d84fc608c5378acfae74d0
Sumit <sumitsainicbl24@gmail.com>
2025-07-31 22:45:46 +0530

    onboarding added

--- src/constants/ImagePath.ts
+++ src/constants/ImagePath.ts
@@ -3,4 +3,12 @@ export default {
     MainLogo_themeLight: require("../assets/Images/MainLogo_themeLight.png"),
     Logo_themeDark: require("../assets/Images/Logo_themeDark.png"),
     Logo_themeLight: require("../assets/Images/Logo_themeLight.png"),
+
+    //OnBoardingIcons
+    OnlineShopping: require("../assets/Images/OnlineShopping.png"),
+    Voucher: require("../assets/Images/Voucher.png"),
+    CardsSample: require("../assets/Images/CardsSample.png"),
+
+    //directions
+    arrowleft: require("../assets/Images/arrowleft.png"),
 };
--- src/screens/auth/Onboarding.tsx
+++ src/screens/auth/Onboarding.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import ImagePath from '../../constants/ImagePath'
 
 const OnBoarding = () => {
   return (
-    <View className='flex-1 justify-center items-center'>
-      
+    <View className='flex-1 px-5'>
+      <View className='flex-1'>
+        <View className='bg-cyan-50 h-3/6 p-5 rounded-3xl'>
+          <View className='flex-row justify-between'>
+            <Image source={ImagePath.Logo_themeLight} className='w-28 h-8' resizeMode='contain' />
+            <Text className='text-2xl font-bold text-cyan-500'>Skip for now</Text>
+          </View>
+          <View className=' flex-1 justify-center items-center'>
+            <Image source={ImagePath.OnlineShopping} className='h-60 w-60' resizeMode='contain' />
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
